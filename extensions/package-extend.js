ddb686e6f76fe5f32ff924f46032b377dc7ff989
Eric Zingeler <eric@ezinterweb.com>
2018-11-11 12:39:07 -0800

    cleanup

--- extensions/package-extend.js
+++ extensions/package-extend.js
@@ -5,8 +5,6 @@ const deepExtend = require('deep-extend');
 module.exports = function(pkgA) {
   const pkgB = this.fs.readJSON(this.destinationPath('package.json')) || {};
 
-  console.log(orderBy(deepExtend(pkgB, pkgA), getOrder()).eslintConfig);
-
   this.fs.writeJSON(
     this.destinationPath('package.json'),
     orderBy(deepExtend(pkgB, pkgA), getOrder())
